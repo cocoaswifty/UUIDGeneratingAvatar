a078c2ccb25483b2f5db7135c14443b81e1d664b
cocoaswifty <cocoaswifty@gmail.com>
2016-08-26 11:57:44 +0800

    init

---
+++ UUIDGeneratingAvatar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UUIDGeneratingAvatar
+//
+//  Created by jianhao on 2016/8/26.
+//  Copyright © 2016年 jianhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
